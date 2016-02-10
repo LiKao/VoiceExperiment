919985b298023bac14a013907eaae71aae58020c
Tillmann Nett <mail@tcrueger.de>
2016-02-10 14:07:53 +0100

    Added a function to analyse a complete directory at once

--- R/analysis.R
+++ R/analysis.R
@@ -17,3 +17,21 @@ analyse.file <- function( filename, channels=c("both","left","right"),
 	w <- read.wav(filename, channels )
 	onsets.WaveData( w, limit, window.width, stepsize, window.function )
 }
+
+
+#' Analyse a Complete Directory for Onset Times
+#' 
+#' @param dirname	Name of the directory from which the files should be taken
+#' @inheritParams read.wav
+#' @inheritParams onsets
+#' @inheritParams energyDensity
+#' 
+#' @export
+analyse.directory <- function(dirname, channels=c("both","left","right"), 
+						  	  limit = 0.1, window.width=10, stepsize=5, window.function=signal::boxcar ) {
+	filenames <- list.files(dirname, pattern="\\.wav")
+	fullnames <- paste(dirname,filenames,sep="/")
+	r <- lapply(fullnames,analyse.file)
+	names(r) <- filenames
+	r
+}
