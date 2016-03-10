f521f1d9d25a1489ab980cb586fc963f91a12261
Tillmann Nett <mail@tcrueger.de>
2016-03-10 13:23:48 +0100

    Added tests for illegal parameters

--- tests/testthat/test_parameters.R
+++ tests/testthat/test_parameters.R
@@ -23,6 +23,142 @@
 
 context("Parameters")
 
+###############################################
+
+test_that("Illegal Paramters produce errors", {
+
+	### Prepare test data
+	
+	w <- read.wav("../testdata/silence_50ms_mono.wav")
+	e <- energyDensity(w)
+			
+	### read.wav
+	
+	expect_error( read.wav("../testdata/illegal.wav"), "File '[^']*' does not exist.")
+	expect_error( read.wav("../testdata/silence_50ms_mono.wav",channels="none"), 
+			      "'arg' should be one of \"both\", \"left\", \"right\"")
+		  
+	### energyDensity
+	
+	expect_error( energyDensity(w, window.width=-10, stepsize=5), 	"Illegal window width: -10")
+	expect_error( energyDensity(w, window.width=0, stepsize=5), 	"Illegal window width: 0")
+	expect_error( energyDensity(w, window.width=10, stepsize=-10), 	"Illegal stepsize: -10")
+	expect_error( energyDensity(w, window.width=10, stepsize=0), 	"Illegal stepsize: 0")
+	expect_warning( energyDensity(w, window.width=10, stepsize=15), "Stepsize 15 is larger than window width 10")
+	
+	
+	### energyDensity.WaveData
+	
+	expect_error( energyDensity.WaveData(w, window.width=-10, stepsize=5), 		"Illegal window width: -10")
+	expect_error( energyDensity.WaveData(w, window.width=0, stepsize=5), 		"Illegal window width: 0")
+	expect_error( energyDensity.WaveData(w, window.width=10, stepsize=-10), 	"Illegal stepsize: -10")
+	expect_error( energyDensity.WaveData(w, window.width=10, stepsize=0), 		"Illegal stepsize: 0")
+	expect_warning( energyDensity.WaveData(w, window.width=10, stepsize=15), 	"Stepsize 15 is larger than window width 10")
+	
+	### onsets
+	
+	expect_error( onsets(w, limit=-2), 		"Illegal limit value: -2" )
+	expect_error( onsets(w, limit=-1), 		"Illegal limit value: -1" )
+	expect_error( onsets(w, limit=-0.1), 	"Illegal limit value: -0.1" )
+	expect_error( onsets(w, limit= 0), 		"Illegal limit value: 0" )
+	expect_error( onsets(w, limit= 1), 		"Illegal limit value: 1" )
+	expect_error( onsets(w, limit= 2), 		"Illegal limit value: 2" )
+	
+	expect_error( onsets(e, limit=-2), 		"Illegal limit value: -2" )
+	expect_error( onsets(e, limit=-1), 		"Illegal limit value: -1" )
+	expect_error( onsets(e, limit=-0.1), 	"Illegal limit value: -0.1" )
+	expect_error( onsets(e, limit= 0), 		"Illegal limit value: 0" )
+	expect_error( onsets(e, limit= 1), 		"Illegal limit value: 1" )
+	expect_error( onsets(e, limit= 2), 		"Illegal limit value: 2" )
+	
+	### onsets.WaveData
+	
+	expect_error( onsets.WaveData(w, limit=-2, window.width=10, stepsize=5), 	"Illegal limit value: -2" )
+	expect_error( onsets.WaveData(w, limit=-1, window.width=10, stepsize=5), 	"Illegal limit value: -1" )
+	expect_error( onsets.WaveData(w, limit=-0.1, window.width=10, stepsize=5), 	"Illegal limit value: -0.1" )
+	expect_error( onsets.WaveData(w, limit= 0, window.width=10, stepsize=5), 	"Illegal limit value: 0" )
+	expect_error( onsets.WaveData(w, limit= 1, window.width=10, stepsize=5), 	"Illegal limit value: 1" )
+	expect_error( onsets.WaveData(w, limit= 2, window.width=10, stepsize=5), 	"Illegal limit value: 2" )
+	
+	expect_error( onsets.WaveData(w, limit=0.1, window.width=-10, stepsize= 5), 	"Illegal window width: -10")
+	expect_error( onsets.WaveData(w, limit=0.1, window.width=  0, stepsize= 5), 	"Illegal window width: 0")
+	expect_error( onsets.WaveData(w, limit=0.1, window.width= 10, stepsize=-10), 	"Illegal stepsize: -10")
+	expect_error( onsets.WaveData(w, limit=0.1, window.width= 10, stepsize=  0), 	"Illegal stepsize: 0")
+	expect_warning( onsets.WaveData(w, limit=0.1, window.width=10, stepsize=15),	"Stepsize 15 is larger than window width 10")
+	
+	### onsets.energyDensity
+	
+	expect_error( onsets.energyDensity(e, limit=-2), 	"Illegal limit value: -2" )
+	expect_error( onsets.energyDensity(e, limit=-1), 	"Illegal limit value: -1" )
+	expect_error( onsets.energyDensity(e, limit=-0.1), 	"Illegal limit value: -0.1" )
+	expect_error( onsets.energyDensity(e, limit= 0), 	"Illegal limit value: 0" )
+	expect_error( onsets.energyDensity(e, limit= 1), 	"Illegal limit value: 1" )
+	expect_error( onsets.energyDensity(e, limit= 2), 	"Illegal limit value: 2" )
+	
+	
+	### analyse.file 
+	
+	expect_error( analyse.file("../testdata/illegal.wav", channels="both", limit = 0.1, window.width=10, stepsize=5), 
+				  "File '[^']*' does not exist.")
+	expect_error( analyse.file("../testdata/silence_50ms_mono.wav",channels="none", limit = 0.1, window.width=10, stepsize=5), 
+				  "'arg' should be one of \"both\", \"left\", \"right\"")
+
+	expect_error( analyse.file("../testdata/silence_50ms_mono.wav",channels="both", limit = -2, window.width=10, stepsize=5),
+				  "Illegal limit value: -2" )
+	expect_error( analyse.file("../testdata/silence_50ms_mono.wav",channels="both", limit = -1, window.width=10, stepsize=5),
+				  "Illegal limit value: -1" )
+	expect_error( analyse.file("../testdata/silence_50ms_mono.wav",channels="both", limit = -0.1, window.width=10, stepsize=5),
+			      "Illegal limit value: -0.1" )
+	expect_error( analyse.file("../testdata/silence_50ms_mono.wav",channels="both", limit =  0, window.width=10, stepsize=5),
+				  "Illegal limit value: 0" )
+	expect_error( analyse.file("../testdata/silence_50ms_mono.wav",channels="both", limit =  1, window.width=10, stepsize=5),
+			      "Illegal limit value: 1" )
+	expect_error( analyse.file("../testdata/silence_50ms_mono.wav",channels="both", limit =  2, window.width=10, stepsize=5),
+			      "Illegal limit value: 2" )
+		  
+	expect_error( analyse.file("../testdata/silence_50ms_mono.wav",channels="both", limit =  0.1, window.width=-10, stepsize=  5),
+				  "Illegal window width: -10")
+	expect_error( analyse.file("../testdata/silence_50ms_mono.wav",channels="both", limit =  0.1, window.width=  0, stepsize=  5),
+				  "Illegal window width: 0")
+	expect_error( analyse.file("../testdata/silence_50ms_mono.wav",channels="both", limit =  0.1, window.width= 10, stepsize=-10),
+			      "Illegal stepsize: -10")
+	expect_error( analyse.file("../testdata/silence_50ms_mono.wav",channels="both", limit =  0.1, window.width= 10, stepsize=  0),
+			      "Illegal stepsize: 0")
+	expect_warning( analyse.file("../testdata/silence_50ms_mono.wav",channels="both", limit =  0.1, window.width=10, stepsize=15),
+			        "Stepsize 15 is larger than window width 10")
+			
+	### analyse.directory	
+	expect_error( analyse.directory("../testdata/illegal/", channels="both", limit = 0.1, window.width=10, stepsize=5), 
+			      "Directory '[^']*' does not exist.")
+	expect_error( analyse.directory("../testdata/testsets",channels="none", limit = 0.1, window.width=10, stepsize=5), 
+			      "'arg' should be one of \"both\", \"left\", \"right\"")
+	
+	expect_error( analyse.directory("../testdata/testsets",channels="both", limit = -2, window.width=10, stepsize=5),
+			      "Illegal limit value: -2" )
+	expect_error( analyse.directory("../testdata/testsets",channels="both", limit = -1, window.width=10, stepsize=5),
+			      "Illegal limit value: -1" )
+	expect_error( analyse.directory("../testdata/testsets",channels="both", limit = -0.1, window.width=10, stepsize=5),
+			      "Illegal limit value: -0.1" )
+	expect_error( analyse.directory("../testdata/testsets",channels="both", limit =  0, window.width=10, stepsize=5),
+			      "Illegal limit value: 0" )
+	expect_error( analyse.directory("../testdata/testsets",channels="both", limit =  1, window.width=10, stepsize=5),
+			      "Illegal limit value: 1" )
+	expect_error( analyse.directory("../testdata/testsets",channels="both", limit =  2, window.width=10, stepsize=5),
+			      "Illegal limit value: 2" )
+	
+	expect_error( analyse.directory("../testdata/testsets",channels="both", limit =  0.1, window.width=-10, stepsize=  5),
+			      "Illegal window width: -10")
+	expect_error( analyse.directory("../testdata/testsets",channels="both", limit =  0.1, window.width=  0, stepsize=  5),
+			      "Illegal window width: 0")
+	expect_error( analyse.directory("../testdata/testsets",channels="both", limit =  0.1, window.width= 10, stepsize=-10),
+			      "Illegal stepsize: -10")
+	expect_error( analyse.directory("../testdata/testsets",channels="both", limit =  0.1, window.width= 10, stepsize=  0),
+			      "Illegal stepsize: 0")
+	expect_warning( analyse.directory("../testdata/testsets",channels="both", limit =  0.1, window.width=10, stepsize=15),
+			        "Stepsize 15 is larger than window width 10")
+	
+})
+
 #############################################################
 
 test_that("Parameters are propagated during file analysis", {
